f998d45240703ff006bc9071eeea1d523066e533
shubhamdz <shubhamkumar12000@gmail.com>
2026-08-16 17:26:30 +0530

    feat: add render.yaml for deployment, update redis cache configuration and create local redis cache fallback

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,17 +1,18 @@
+import os
 import redis
-import json
-from app.core.config import settings
+from dotenv import load_dotenv
 
+load_dotenv()
 
-redis_client = redis.Redis.from_url(settings.REDIS_URL)
+REDIS_URL = os.getenv("REDIS_URL")
+
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 
 def get_cached_prediction(key: str):
     value = redis_client.get(key)
-    if value:
-        return json.loads(value)
-    return None
+    return eval(value) if value else None
 
 
-def set_cached_prediction(key: str, value: dict, ex: int = 3600):
-    redis_client.setex(key, ex, json.dumps(value))
+def set_cached_prediction(key: str, value: dict):
+    redis_client.set(key, str(value))
--- app/services/model_service.py
+++ app/services/model_service.py
@@ -1,7 +1,7 @@
 import joblib
 import pandas as pd
 from app.core.config import settings
-from app.cache.redis_cache import set_cached_prediction, get_cached_prediction
+from app.cache.redis_cache_local import set_cached_prediction, get_cached_prediction
 
 model = joblib.load(settings.MODEL_PATH)
 
